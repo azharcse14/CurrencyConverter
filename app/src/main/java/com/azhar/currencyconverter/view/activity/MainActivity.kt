839615e7e728505bfaff3c4f852feb9d1e0ef120
azharul <mdazharcse14@gmail.com>
2022-05-23 18:16:08 +0600

    Change package activity main

--- app/src/main/java/com/azhar/currencyconverter/view/activity/MainActivity.kt
+++ app/src/main/java/com/azhar/currencyconverter/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.azhar.currencyconverter
+package com.azhar.currencyconverter.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.azhar.currencyconverter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
