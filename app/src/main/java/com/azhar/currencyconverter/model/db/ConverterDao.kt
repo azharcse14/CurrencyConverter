de6e04900fb102876c107eb48a26025841928f0c
azharul <mdazharcse14@gmail.com>
2022-05-23 23:32:43 +0600

    Update dao

--- app/src/main/java/com/azhar/currencyconverter/model/db/ConverterDao.kt
+++ app/src/main/java/com/azhar/currencyconverter/model/db/ConverterDao.kt
@@ -14,24 +14,24 @@ interface ConverterDao {
     suspend fun addCurrencies(currencies: Currency)
 
     @Query("SELECT * FROM currencies")
-    fun getCurrencies() : LiveData<List<Currency>>
+    fun getCurrencies(): LiveData<List<Currency>>
 
     @Query("SELECT 'key' from currencies")
-    fun getCurrencieskey() : LiveData<List<String>>
+    fun getCurrencieskey(): LiveData<List<String>>
 
     @Query("SELECT value from currencies")
-    fun getCurrenciesValue() : LiveData<List<String>>
+    fun getCurrenciesValue(): LiveData<List<String>>
 
     @Insert
     suspend fun addQuotes(quotes: Quote)
 
     @Query("SELECT * FROM quotes")
-    suspend fun getQuotes() : Quote
+    suspend fun getQuotes(): Quote
 
     @Query("SELECT quote_value FROM quotes where quote_key=:key")
-    fun getQuoteValue(key:String) : Double
+    fun getQuoteValue(key: String): Double
 
     @Query("SELECT quote_key, quote_value FROM quotes")
-    fun getAllQuote() : List<Quote>
+    fun getAllQuote(): List<Quote>
 
 }
