31193ac0ce0ce59fd7e0dd8ee8a73fa410861ce5
azharul <mdazharcse14@gmail.com>
2022-05-23 18:34:55 +0600

    Create Dao and database

---
+++ app/src/main/java/com/azhar/currencyconverter/model/db/ConverterDao.kt
@@ -0,0 +1,33 @@
+package com.azhar.currencyconverter.model.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.azhar.currencyconverter.model.data.local.Currency
+import com.azhar.currencyconverter.model.data.local.Quote
+
+@Dao
+interface ConverterDao {
+
+    @Insert
+    suspend fun addCurrencies(currencies: Currency)
+
+    @Query("SELECT * FROM currencies")
+    fun getCurrencies() : LiveData<List<Currency>>
+
+    @Query("SELECT 'key' from currencies")
+    fun getCurrencieskey() : LiveData<List<String>>
+
+    @Query("SELECT value from currencies")
+    fun getCurrenciesValue() : LiveData<List<String>>
+
+    @Insert
+    suspend fun addQuotes(quotes: Quote)
+
+    @Query("SELECT * FROM quotes")
+    suspend fun getQuotes() : Quote
+
+    @Query("SELECT quote_value FROM quotes where quote_key=:key")
+    fun getQuoteValue(key:String) : LiveData<String>
+}
---
+++ app/src/main/java/com/azhar/currencyconverter/model/db/ConverterDatabase.kt
@@ -0,0 +1,31 @@
+package com.azhar.currencyconverter.model.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.azhar.currencyconverter.model.data.local.Currency
+import com.azhar.currencyconverter.model.data.local.Quote
+
+@Database(entities = [Currency::class, Quote::class], version = 1)
+abstract class ConverterDatabase : RoomDatabase() {
+
+    abstract fun ConverterDB() : ConverterDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ConverterDatabase? = null
+
+        fun getDatabase(context: Context): ConverterDatabase {
+            if (INSTANCE == null) {
+                synchronized(this){
+                    INSTANCE = Room.databaseBuilder(context,
+                        ConverterDatabase::class.java,
+                        "ConverterDB")
+                        .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
