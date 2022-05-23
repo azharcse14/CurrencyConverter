12dcbbd11ec44e18cc537eb0a7dd3f8d18fea5ab
azharul <mdazharcse14@gmail.com>
2022-05-23 23:37:20 +0600

    reformet

--- app/src/main/java/com/azhar/currencyconverter/viewModel/MainViewModelFactory.kt
+++ app/src/main/java/com/azhar/currencyconverter/viewModel/MainViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.azhar.currencyconverter.viewModel
 
-class MainViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.azhar.currencyconverter.model.repository.Repository
+
+class MainViewModelFactory(private val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(repository) as T
+    }
+
 }
