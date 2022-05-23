623382e0b985c1366e477ea1a43e58d61beab10a
azharul <mdazharcse14@gmail.com>
2022-05-23 23:33:38 +0600

    reformet

--- app/src/main/java/com/azhar/currencyconverter/model/network/ApiService.kt
+++ app/src/main/java/com/azhar/currencyconverter/model/network/ApiService.kt
@@ -1,7 +1,7 @@
 package com.azhar.currencyconverter.model.network
 
 import com.azhar.currencyconverter.model.data.network.CurrencyList
-import com.azhar.currencyconverter.model.data.network.QuoetsList
+import com.azhar.currencyconverter.model.data.network.QuoteList
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -10,12 +10,12 @@ interface ApiService {
 
     @Headers("apikey: 8hfNZ1wKRuiF0FyqXYiXjXidtKV3cggV")
     @GET("list")
-    suspend fun getCurrencyList() : CurrencyList
+    suspend fun getCurrencyList(): CurrencyList
 
     @Headers("apikey: 8hfNZ1wKRuiF0FyqXYiXjXidtKV3cggV")
     @GET("live")
     suspend fun getQuotes(
-        @Query("source") source:String,
-        @Query("currencies") currencies:String
-    ) : QuoetsList
+        @Query("source") source: String,
+        @Query("currencies") currencies: String
+    ): QuoteList
 }
